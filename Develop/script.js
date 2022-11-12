9c796db9487254a1ba1739dcdb44ebe97b051430
tmalenga0@gmail.com <tmalenga0@gmail.com>
2022-11-12 09:29:58 -0400

    Add starter code and comments

---
+++ Develop/script.js
@@ -0,0 +1,27 @@
+/*
+- Welcome user to program
+- Create prompt for the lenghth of the pw & save this in a variable
+- While loop to ensure that a user picks an appropriate lenghth of pw
+- Create statement to evaluate lenght of pw i.e. to be between 8 - 128 characters and save this in a variable
+- Create prompt for the char types to incl in the pw (lowercase, uppercase, numeric &/ special chars) & save in variable
+- for loop to add the characters that the user selected into the password. Loop to run for the length of the pw selected by user
+- ****
+*/
+
+// Assignment code here
+
+
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
